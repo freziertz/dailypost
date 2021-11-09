f90b3b8c45271608794cd11579cdd7e47c518968
frezier <freziern@gmail.com>
2021-11-09 15:42:43 +0300

    8311c6e

---
+++ _nuxt/static/1636461717/fun/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdailypost\u002F_nuxt\u002Fstatic\u002F1636461717",layout:"blog",error:a,serverRendered:true,routePath:"\u002Ffun",config:{_app:{basePath:"\u002Fdailypost\u002F",assetsPath:"\u002Fdailypost\u002F_nuxt\u002F",cdnURL:a}}}}(null));
